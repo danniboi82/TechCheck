b2bd2d787afa758a9be9fcbce912d0f5b7c7f0b5
Unknown <danoh82@gmail.com>
2018-01-16 14:38:06 -0800

    Slight changes
    changed sandbox token for paypal express 
    fixed route to work with Kevin's routes

--- client/src/Components/CheckOutPage/PayPalButton/PayPalButton.jsx
+++ client/src/Components/CheckOutPage/PayPalButton/PayPalButton.jsx
@@ -5,7 +5,7 @@ import PaypalExpressBtn from 'react-paypal-express-checkout';
 export default class PayPalButton extends Component {
     render() {
         const client = {
-            sandbox:    'XXXXXXXXXX',
+            sandbox:    'access_token$sandbox$jhj9j3nyvsqckspr$cae9c8326b09b9d92e5b1c3bb1e4a758',
             production: 'YOUR-PRODUCTION-APP-ID',
         }	
         return (
