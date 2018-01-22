54be560dd2530110e730f830474098bd5f88264d
Unknown <danoh82@gmail.com>
2018-01-21 17:28:45 -0800

    Added Add to cart icon on searched products
    also deleted unecessary files

--- client/src/Components/SearchedPage/SearchResults/SearchResults.jsx
+++ client/src/Components/SearchedPage/SearchResults/SearchResults.jsx
@@ -1,10 +1,23 @@
-import React,{Component} from 'react';
-import {GridList, GridTile} from 'material-ui/GridList';
+import React, { Component } from 'react';
+import { GridList, GridTile } from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import productsApi from '../../Data/products-api'
 import axios from "axios";
+import { greenA200 } from 'material-ui/styles/colors';
+import SvgIcon from 'material-ui/SvgIcon';
+
+const CartIcon = (props) => (
+  <SvgIcon {...props}>
+    <svg fill="#FFFFF" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0h24v24H0zm18.31 6l-2.76 5z" fill="none" />
+      <path d="M11 9h2V6h3V4h-3V1h-2v3H8v2h3v3zm-4 9c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2zm-9.83-3.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.86-7.01L19.42 4h-.01l-1.1 2-2.76 5H8.53l-.13-.27L6.16 6l-.95-2-.94-2H1v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.13 0-.25-.11-.25-.25z" />
+    </svg>
+  </SvgIcon>
+)
+
+
 const styles = {
   root: {
     display: 'flex',
@@ -19,7 +32,7 @@ const styles = {
   },
   subheader: {
     fontSize: '50px',
-    color: 'black', 
+    color: 'black',
     padding: '50px 0',
     backgroundColor: 'red',
     textAlign: 'center'
@@ -72,108 +85,109 @@ const dummyData = [
 /**
  * A simple example of a scrollable `GridList` containing a [Subheader](/#/components/subheader).
  */
-class ProductSearch extends Component{
-  state={
-    products:[],
-    pages:0,
-    limit:15
+class ProductSearch extends Component {
+  state = {
+    products: [],
+    pages: 0,
+    limit: 15
   }
-  componentDidMount=()=>{
+  componentDidMount = () => {
 
     console.log(this.props.match.params.category)
     axios({
-      method:'post',
-     url: `/api/products/category`,
-     data:{
-      category:this.props.match.params.category,
-      page:0,
-      limit:15
+      method: 'post',
+      url: `/api/products/category`,
+      data: {
+        category: this.props.match.params.category,
+        page: 0,
+        limit: 15
 
-     }
-  })
-.then(products=>{
-  console.log(products)
-  this.setState({
-    products: products.data,
-    
-})
-})
-}
-    // userId:this.props.match.params.id,
-           
-    // productsApi.catagorySearch(this.props.match.params.category)
-  console=()=>{
-    console.log( this.state.page)
-     axios({
-         method:'post',
-        url: `/api/products/category`,
-        data:{
-            category:this.props.match.params.category,
-            page:this.state.page,
-            limit:this.state.limit
+      }
+    })
+      .then(products => {
+        console.log(products)
+        this.setState({
+          products: products.data,
 
-        }
-     }).then(next=>{
-console.log(next)
-this.setState({
- products: next.data
-},console.log(this.state.products))
-
-     })
-}
-limit=(e)=>{
-
-console.log(e.currentTarget.attributes.value.nodeValue)
-    this.setState({
-        
-        limit:e.currentTarget.attributes.value.nodeValue
-    },this.console)
-  
-    
-}
-pages=(e)=>{
-
-
-this.setState({
-    page:e.currentTarget.attributes.value.nodeValue,
- 
-},this.console)
-
-
-}
-  render(){
-
-  return (
-  <div style={styles.root}>
-   <Subheader style={styles.subheader}>Search Results</Subheader>
-   <p>Results per page:</p>
-                <button onClick={this.limit} value={15}>15</button><button onClick={this.limit}value={30}>30</button>
-    <GridList
-      cellHeight={180}
-      style={styles.gridList}
-      cols={4}
-      padding={10}
-    >
-     
-      {this.state.products.map((tile) => (
-        <GridTile
-          key={tile.id}
-          title={tile.productName}
-          price={tile.price
-          }
-          subtitle={<span>Price Range<b>{tile.price}</b></span>}
-          actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-        >
-          <img src={`https://s3-us-west-1.amazonaws.com/techcheckbucket/${tile.userUploadImage1}`} alt='Searched Products'/>
-        </GridTile>
-      ))}<br/>
-        <div className='pages'>
-                <button onClick={this.pages} name='1'value={0} >1</button><button onClick={this.pages}name='2' value={15} >2</button> <button onClick={this.pages}name='3' value={30} >3</button> <button onClick={this.pages} value={45} >4</button> <button onClick={this.pages} value={60} >5</button> 
-                </div>
-    </GridList>
-  </div>
-  )
-}
+        })
+      })
   }
+  // userId:this.props.match.params.id,
+
+  // productsApi.catagorySearch(this.props.match.params.category)
+  console = () => {
+    console.log(this.state.page)
+    axios({
+      method: 'post',
+      url: `/api/products/category`,
+      data: {
+        category: this.props.match.params.category,
+        page: this.state.page,
+        limit: this.state.limit
+
+      }
+    }).then(next => {
+      console.log(next)
+      this.setState({
+        products: next.data
+      }, console.log(this.state.products))
+
+    })
+  }
+  limit = (e) => {
+
+    console.log(e.currentTarget.attributes.value.nodeValue)
+    this.setState({
+
+      limit: e.currentTarget.attributes.value.nodeValue
+    }, this.console)
+
+
+  }
+  pages = (e) => {
+
+
+    this.setState({
+      page: e.currentTarget.attributes.value.nodeValue,
+
+    }, this.console)
+
+
+  }
+  render() {
+
+    return (
+      <div style={styles.root}>
+        <Subheader style={styles.subheader}>Search Results</Subheader>
+        <p>Results per page:</p>
+        <button onClick={this.limit} value={15}>15</button><button onClick={this.limit} value={30}>30</button>
+        <GridList
+          cellHeight={180}
+          style={styles.gridList}
+          cols={4}
+          padding={10}
+        >
+
+          {this.state.products.map((tile) => (
+            <GridTile
+              key={tile.id}
+              title={tile.productName}
+              price={tile.price
+              }
+              subtitle={<span>Price Range<b>{tile.price}</b></span>}
+              actionIcon={<IconButton><CartIcon/></IconButton>}
+              titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+            >
+              <img src={`https://s3-us-west-1.amazonaws.com/techcheckbucket/${tile.userUploadImage1}`} alt='Searched Products' />
+            </GridTile>
+          ))}<br />
+          <div className='pages'>
+            <button onClick={this.pages} name='1' value={0} >1</button><button onClick={this.pages} name='2' value={15} >2</button> <button onClick={this.pages} name='3' value={30} >3</button> <button onClick={this.pages} value={45} >4</button> <button onClick={this.pages} value={60} >5</button>
+          </div>
+        </GridList>
+      </div>
+    )
+  }
+}
 
 export default ProductSearch;
--- client/src/Components/usersPages/UserProducts/UserProducts.jsx
+++ client/src/Components/usersPages/UserProducts/UserProducts.jsx
@@ -4,88 +4,88 @@ import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 import './UserProducts.css';
 import products1 from '../../Data/products-api';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from "axios";
 
 class userProducts extends Component {
 
     state = {
         userProducts: [],
-        userId:"",
-        s3ornot:false,
-        userUploadImage1:[],
-        page:'',
-        limit:""
+        userId: "",
+        s3ornot: false,
+        userUploadImage1: [],
+        page: '',
+        limit: ""
 
     }
 
     componentDidMount = () => {
 
-       // console.log('this is my test')
-let id=this.props.match.params.id
+        // console.log('this is my test')
+        let id = this.props.match.params.id
 
-       console.log(id)
-       axios({
-        method:'post',
-       url: `/api/products/user/products`,
-       data:{
-           userId:this.props.match.params.id,
-           page:0,
-           limit:15
+        console.log(id)
+        axios({
+            method: 'post',
+            url: `/api/products/user/products`,
+            data: {
+                userId: this.props.match.params.id,
+                page: 0,
+                limit: 15
 
-       }
-    }).then(dataPoints => {
-           
-           // console.log(dataPoints)
-        this.setState({
-            userProducts: dataPoints.data,
-           userId:this.props.match.params.id,
-            userUploadImage1:dataPoints.data.userUploadImage1,
-            page:0,
-        })
-           // console.log(this.state.userProducts)
-            
-            console.log(Math.floor(new Date().getTime() / 1000)/1000)
+            }
+        }).then(dataPoints => {
+
+            // console.log(dataPoints)
+            this.setState({
+                userProducts: dataPoints.data,
+                userId: this.props.match.params.id,
+                userUploadImage1: dataPoints.data.userUploadImage1,
+                page: 0,
+            })
+            // console.log(this.state.userProducts)
+
+            console.log(Math.floor(new Date().getTime() / 1000) / 1000)
         });
     }
-    console=()=>{
-        console.log( this.state.limit)
-         axios({
-             method:'post',
+    console = () => {
+        console.log(this.state.limit)
+        axios({
+            method: 'post',
             url: `/api/products/user/products`,
-            data:{
-                userId:this.props.match.params.id,
-                page:this.state.page,
-                limit:this.state.limit
-   
+            data: {
+                userId: this.props.match.params.id,
+                page: this.state.page,
+                limit: this.state.limit
+
             }
-         }).then(next=>{
- console.log(next)
- this.setState({
-     userProducts: next.data
- })
-         })
+        }).then(next => {
+            console.log(next)
+            this.setState({
+                userProducts: next.data
+            })
+        })
     }
-    limit=(e)=>{
+    limit = (e) => {
 
-console.log(e.currentTarget.attributes.value.nodeValue)
+        console.log(e.currentTarget.attributes.value.nodeValue)
         this.setState({
-            
-            limit:e.currentTarget.attributes.value.nodeValue
-        },this.console)
-      
-        
+
+            limit: e.currentTarget.attributes.value.nodeValue
+        }, this.console)
+
+
     }
-pages=(e)=>{
+    pages = (e) => {
 
 
-    this.setState({
-        page:e.currentTarget.attributes.value.nodeValue,
-     
-    },this.console)
-    
-    
-}
+        this.setState({
+            page: e.currentTarget.attributes.value.nodeValue,
+
+        }, this.console)
+
+
+    }
     deleteProductHandler = (id) => {
         const ogProductList = this.state.userProducts;
         ogProductList.splice(id, 1);
@@ -101,9 +101,9 @@ pages=(e)=>{
             <div className='CheckOutPageDiv'>
                 <div>
                     <h1>User Products</h1>
-                    <br/>
+                    <br />
                     <p>Results per page</p>
-                <button onClick={this.limit} value={15}>15</button><button onClick={this.limit}value={30}>30</button>
+                    <button onClick={this.limit} value={15}>15</button><button onClick={this.limit} value={30}>30</button>
                 </div>
                 {/* {console.log('HELLO', this.state.userProducts)} */}
                 {this.state.userProducts.map((card, id) => (
@@ -125,40 +125,10 @@ pages=(e)=>{
                 ))}
 
 
- 
 
-   <div>
-     {/* <div>Device Test!</div>
-     <MediaQuery minDeviceWidth={1224} values={{ deviceWidth: 1600 }}>
-       <div>You are a desktop or laptop</div>
-       <MediaQuery minDeviceWidth={1824}>
-         <div>You also have a huge screen</div>
-       </MediaQuery>
-       <MediaQuery maxWidth={1224}>
-         <div>You are sized like a tablet or mobile phone though</div>
-       </MediaQuery>
-     </MediaQuery>
-     <MediaQuery maxDeviceWidth={1224}>
-       <div>You are a tablet or mobile phone</div>
-     </MediaQuery>
-     <MediaQuery orientation="portrait">
-       <div>You are portrait</div>
-     </MediaQuery>
-     <MediaQuery orientation="landscape">
-       <div>You are landscape</div>
-     </MediaQuery>
-     <MediaQuery minResolution="2dppx">
-       <div>You are retina</div>
-<<<<<<< HEAD
-     </MediaQuery>
-=======
-     </MediaQuery> */}
->>>>>>> hope
-   </div>
- 
                 <div className='pages'>
-                <button onClick={this.pages} name='1'value={0} >1</button><button onClick={this.pages}name='2' value={15} >2</button> <button onClick={this.pages}name='3' value={30} >3</button> <button onClick={this.pages} value={45} >4</button> <button onClick={this.pages} value={60} >5</button> 
-                
+                    <button onClick={this.pages} name='1' value={0} >1</button><button onClick={this.pages} name='2' value={15} >2</button> <button onClick={this.pages} name='3' value={30} >3</button> <button onClick={this.pages} value={45} >4</button> <button onClick={this.pages} value={60} >5</button>
+
                 </div>
                 <div className='AddProductDiv'>
                     <Link to={`/sell_product/${this.state.userId}`}><FlatButton style={{ backgroundColor: 'blue' }} label='Add Product' /></Link>
